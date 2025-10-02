190f399077a52b770512ac862f04f0febb62a946
Sowmitra Guha <sowmmochy@gmail.com>
2025-10-02 19:05:05 +0600

    pagination done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css'
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
-import { InputSwitch } from 'primereact/inputswitch';
 import { Button } from 'primereact/button';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+
 
 interface products {
   id: number;
@@ -16,22 +16,90 @@ interface products {
   date_end: number;
 }
 
-
 function App() {
   const [products, setProducts] = useState<products[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
-  const [rowClick, setRowClick] = useState<boolean>(true);
   const [selectedProducts, setSelectedProducts] = useState<products[] | null>(null);
   const [totalRecords, setTotalRecords] = useState<number>(0);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [totalPage, setTotalPage] = useState<number>(0);
 
-  const paginatorLeft = <Button type="button" icon="pi pi-refresh" text />;
-  const paginatorRight = <Button type="button" icon="pi pi-download" text />;
+
+
+  const template = {
+    
+    layout: 'PrevPageLink PageLinks NextPageLink CurrentPageReport',
+    PrevPageLink: (options: any) => {
+      
+      const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+        console.log("Previous page clicked");
+        handlePageChange({ page: currentPage - 2 });
+      };
+      return (
+        <button type="button" className='p-paginator-prev p-paginator-element p-link' onClick={handleClick} disabled={currentPage===1}>
+          <span className="mx-1">Previous</span>
+        </button>
+      );
+    },
+    NextPageLink: (options: any) => {
+      //console.log(options);
+      const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+        console.log("Next page clicked");
+        handlePageChange({ page: currentPage });
+      };
+      return (
+        <button type="button" className={options.className} onClick={handleClick} disabled={options.disabled}>
+          <span className="mx-1">Next</span>
+        </button>
+      );
+    },
+    PageLinks: (options: any) => {
+
+      if ((options.view.startPage === options.page && options.view.startPage !== 0) || (options.view.endPage === options.page && options.page + 1 !== options.totalPages)) {
+        const className = `${options.className} p-disabled`;
+        console.log(options);
+        return (
+          <span className={className} style={{ userSelect: 'none' }}>
+            ...
+          </span>
+        );
+      }
+
+      return (
+        <button type="button" className={options.className} onClick={options.onClick}>
+          {options.page + 1}
+        </button>
+      );
+    }
+  }
+
+
+
+
+
+
+
+  const handlePageChange = (event: { first: number; rows: number; page: number; pageCount: number }) => {
+    setLoading(true);
+    axios.get(`https://api.artic.edu/api/v1/artworks?page=${event.page + 1}`)
+      .then(res => {
+        setTotalRecords(res.data.pagination.total);
+        setProducts(res.data.data);
+        setCurrentPage(event.page + 1);
+      })
+      .catch(err => console.error(err))
+      .finally(() => setLoading(false));
+  }
+
+
+
 
   useEffect(() => {
     axios.get("https://api.artic.edu/api/v1/artworks?page=1")
       .then(res => {
         console.log(res.data.pagination.total);
         setTotalRecords(res.data.pagination.total);
+        setTotalPage(res.data.pagination.total_pages);
         setProducts(res.data.data);
       })
       .catch(err => console.error(err))
@@ -44,30 +112,30 @@ function App() {
 
   return (
     <>
-       <div className='container mx-auto p-12'>
-        <div className="flex justify-content-center align-items-center mb-4 gap-2">
-          <InputSwitch inputId="input-rowclick" checked={rowClick} onChange={(e) => setRowClick(e.value)} />
-          <label htmlFor="input-rowclick">Row Click</label>
-        </div>
+      <div className='container mx-auto p-12'>
         <h1 className='my-4'>Table with Pagination</h1>
-        <DataTable 
-        loading={loading}
-        value={products} 
-        size={'normal'} 
-        showGridlines 
-        selectionMode={rowClick ? null : 'checkbox'} 
-        selection={selectedProducts} 
-        onSelectionChange={(e) => setSelectedProducts(e.value)} 
-        tableStyle={{ minWidth: '50rem' }} 
-        paginator 
-         
-        totalRecords={totalRecords}
-        rows={12}
-        
+
+
+        <DataTable
+          loading={loading}
+          value={products}
+          size={'normal'}
+          showGridlines
+          selectionMode='checkbox'
+          selection={selectedProducts}
+          onSelectionChange={(e) => setSelectedProducts(e.value)}
+          tableStyle={{ minWidth: '50rem' }}
+          paginator
+          lazy
+          totalRecords={totalRecords}
+          rows={12}
+          onPage={(e) => { handlePageChange(e) }}
+          paginatorTemplate={template}
+          currentPageReportTemplate={`Showing ${currentPage} of ${totalPage} pages`}
         >
           <Column selectionMode="multiple" style={{ width: '5%' }}></Column>
           <Column field="title" header="Title" style={{ width: '15%' }}></Column>
-          <Column field="place_of_origin" header="Place of Origin" style={{ width: '5%' }}></Column>
+          <Column field="place_of_origin" header="Place of Origin" style={{ width: '15%' }}></Column>
           <Column field="artist_display" header="Artist" style={{ width: '15%' }}></Column>
           <Column field="inscription" header="Inscription" style={{ width: '20%' }}></Column>
           <Column field="date_start" header="Start Date" style={{ width: '10%' }}></Column>
